c55745f2d3bcad6dbe313723ba6aaa5b5ab1dae2
Joe Njung'e <joesnjunge@live.com>
2019-06-18 23:40:28 +0300

    Day1_2

--- src/dashboard-widgets/dashboard-widgets.tsx
+++ src/dashboard-widgets/dashboard-widgets.tsx
@@ -4,7 +4,10 @@ import Parcel from "single-spa-react/parcel";
 
 const patientDashboardParcels = [
   // A patient dashboard widget within this git repo
-  () => import("./basic-info/basic-info.parcel").then(m => m.default),
+  () => {
+    return import("./basic-info/basic-info.parcel").then(m => m.default);
+  },
+  () => System.import("@openmrs/njunge-dashboard-widget")
 ];
 
 export default function DashboardWidgets(props: DashboardWidgetsProps) {
@@ -39,7 +42,7 @@ export default function DashboardWidgets(props: DashboardWidgetsProps) {
 type DashboardWidgetsProps = {
   match: {
     params: {
-      patientUuid: string,
-    }
-  }
+      patientUuid: string;
+    };
+  };
 };
